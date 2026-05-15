5f34367a85b6350b7dd62622cd8e6224140874b6
Flávia Queiroz <flavia.vgq@gmail.com>
2026-05-15 17:02:10 -0300

    Add files via upload

---
+++ frontend/js/script.js
@@ -0,0 +1,107 @@
+const tbody = document.querySelector('tbody');
+const addForm = document.querySelector('.add-form');
+const inputTask = document.querySelector('.input-class');
+
+
+const fetchTasks = async() =>{
+    const response = await fetch('http://localhost:3333/tasks');
+    const tasks = await response.json()
+    return tasks;
+}
+
+
+const addTask = async (event) => {
+    event.preventDefault();
+    const task = { title: inputTask.value
+
+    };
+
+    await fetch ('http://localhost:3333/tasks', {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(task),
+
+    });
+
+
+    loadTasks();
+    inputTask.value = '';
+
+}
+
+const createElement = (tag, innerText = '', innerHTML = '') => {
+    const element = document.createElement(tag);
+
+    if (innerText) {
+        element.innerText = innerText;
+    }
+
+    if (innerHTML) {
+        element.innerHTML = innerHTML;
+    }
+    
+    return element;
+}
+
+const formatDate = (dateUTC) => {
+    const options = {dateStyles:'long', timeStyle:'short'}
+    const date = new Date(dateUTC).toLocaleString('pt-br', options);
+
+    
+    return date;
+}
+
+
+const createSelect = (value) => {
+    const options = ` 
+    <option value = "pendente"> pendente</option>
+    <option value = "em andamento"> em andamento</option>
+    <option value = "concluída"> concluída</option> `;
+
+    const select = createElement('select','', options);
+    
+    return select;
+}
+
+const createRow = (tasks) => {
+    const {id, title, created_at, status} = tasks;
+
+    const tr = createElement('tr')
+    const tdTitle= createElement('td', title)
+    const tdcreated_at = createElement('td', formatDate(created_at))
+    const tdstatus = createElement('td')
+    const tdActions = createElement('td')
+    const select = createSelect(status);
+
+    const editButton = createElement('button', '',' <span class="material-symbols-outlined">edit</span>')
+    const deleteButton = createElement('button', '',' <span class="material-symbols-outlined">delete</span>')
+
+    editButton.classList.add('btn-action');
+    deleteButton.classList.add('btn-action');
+
+    tdActions.appendChild(editButton);
+    tdActions.appendChild(deleteButton);
+    tdstatus.appendChild(select);
+
+    tr.appendChild(tdTitle);
+    tr.appendChild(tdcreated_at);
+    tr.appendChild(tdstatus);
+    tr.appendChild(tdActions);
+
+    return tr;
+}
+
+const loadTasks = async () => {
+    const tasks = await fetchTasks();
+
+    tbody.innerHTML = '';
+    tasks.forEach(task => {
+        const tr = createRow(task);
+        tbody.appendChild(tr);
+    });
+}
+
+addForm.addEventListener('submit', addTask);
+loadTasks();
+
+
